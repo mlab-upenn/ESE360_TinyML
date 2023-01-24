532c552783c00e6795a50a3465a6bc42747cadc8
Tejas Agarwal <tejasag@sea.upenn.edu>
2023-01-24 13:31:59 -0500

    Add KWS depedancy on TF1 using Conv2d

--- TinyML_ESE360/examples/micro_speech/micro_speech.ino
+++ TinyML_ESE360/examples/micro_speech/micro_speech.ino
@@ -75,7 +75,7 @@ void setup() {
   //
   // tflite::AllOpsResolver resolver;
   // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::MicroMutableOpResolver<4> micro_op_resolver(error_reporter);
+  static tflite::MicroMutableOpResolver<5> micro_op_resolver(error_reporter);
   if (micro_op_resolver.AddDepthwiseConv2D() != kTfLiteOk) {
     return;
   }
@@ -86,6 +86,9 @@ void setup() {
     return;
   }
   if (micro_op_resolver.AddReshape() != kTfLiteOk) {
+    return;
+  }
+    if (micro_op_resolver.AddConv2D() != kTfLiteOk) {
     return;
   }
 
